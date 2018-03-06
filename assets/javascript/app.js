06e9d27139a2a9d2319c9cf3d4063d34ef4424aa
Wesley Hanson <wrh1600@gmail.com>
2018-03-05 21:41:08 -0700

    logging player1 and player2

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,8 +12,8 @@ var config = {
 
 //Global variables for game
 
-  var playerOne = false;
-  var playerTwo = false;
+  var playerOne = null;
+  var playerTwo = null;
   var playerOneName = "";
   var playerTwonName = "";
   var userplayerName = "";
@@ -34,13 +34,24 @@ var config = {
 
       playerOne = snapshot.val().playerOne;
       playerOneName = playerOne.name;
-      $("#selectRock").show('slow');
-      $("#selectPaper").show('slow');
-      $("#selectScissor").show('slow');
+      $(".p1selectRock").show('slow');
+      $(".p1selectPaper").show('slow');
+      $(".p1selectScissor").show('slow');
+      $("#p1name-display").text(playerOneName);
+      $("#p1Stats").html("Win: " + playerOne.wins + " Losses: " + playerOne.losses + " Ties: " + playerOne.ties);
     }
 
-    $("#p1name-display").text(playerOneName);
-    $("#p1Stats").html("Win: " + playerOne.wins + " Losses: " + playerOne.losses + " Ties: " + playerOne.ties);
+    if(snapshot.child("playerTwo").exists()) {
+      console.log('player two exists');
+
+      playerTwo = snapshot.val().playerTwo;
+      playerTwoName = playerTwo.name;
+      $(".p2selectRock").show('slow');
+      $(".p2selectPaper").show('slow');
+      $(".p2selectScissor").show('slow');
+      $("#p2name-display").text(playerTwoName);
+      $("#p2Stats").html("Win: " + playerTwo.wins + " Losses: " + playerTwo.losses + " Ties: " + playerTwo.ties);
+    }
     
 
   }, function(errorObject) {
@@ -57,25 +68,39 @@ var config = {
 
       if(($('#user-name').val().trim() !== "") && !(playerOne && playerTwo) ) {
 
-        if(playerOne === false) {
-          console.log('adding player one');
-        }
-      }
+        if(playerOne === null) {
+            console.log('adding player one');
+     
+            userplayerName = $("#user-name").val().trim();
 
-      // YOUR TASK!!!
-      // Code in the logic for storing and retrieving the most recent user.
-      // Don't forget to provide initial data to your Firebase database.
-      userplayerName = $("#user-name").val().trim();
-
-      playerOne = {
-        name: userplayerName,
-        wins: 0,
-        losses: 0,
-        ties: 0,
-        choice: ""
-      };
+            playerOne = {
+              name: userplayerName,
+              wins: 0,
+              losses: 0,
+              ties: 0,
+              choice: ""
+            };
       
+              $('#p1Stats').show('slow');
+              database.ref('players/playerOne').set(playerOne);
+              database.ref('players/playerOne').onDisconnect().remove();
+        } else if( (playerOne !== null) && (playerTwo === null) ){
+          console.log('Adding Player 2');
 
-      database.ref('players/playerOne').set(playerOne);
+          userplayerName = $("#user-name").val().trim();
+
+            playerTwo = {
+              name: userplayerName,
+              wins: 0,
+              losses: 0,
+              ties: 0,
+              choice: ""
+            };
+      
+              $('#p2Stats').show('slow');
+              database.ref('players/playerTwo').set(playerTwo);
+              database.ref('players/playerTwo').onDisconnect().remove();
+        }
+      }        
 
     });
